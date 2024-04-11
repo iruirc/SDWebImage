c771ee5facce19f90824e6d45b01d4c8e4bc4a74
Dmitry Nuzhin <iruirc@gmail.com>
2024-04-11 15:25:33 +0300

    Dynamic library

--- Package.swift
+++ Package.swift
@@ -15,9 +15,11 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SDWebImage",
+            type: .dynamic,
             targets: ["SDWebImage"]),
         .library(
             name: "SDWebImageMapKit",
+            type: .dynamic,
             targets: ["SDWebImageMapKit"])
     ],
     dependencies: [
